f430c9fa1a5b55cc66c724a81072c5aa91bb13b9
iwedding <dlwls5201@naver.com>
2020-01-10 09:53:12 +0900

    setting project

---
+++ app/src/main/java/com/tistory/blackjin/metaweatherapp/base/BaseViewModel.kt
@@ -0,0 +1,14 @@
+package com.tistory.blackjin.metaweatherapp.base
+
+import androidx.lifecycle.ViewModel
+import io.reactivex.disposables.CompositeDisposable
+
+abstract class BaseViewModel : ViewModel() {
+
+    val compositeDisposable = CompositeDisposable()
+
+    override fun onCleared() {
+        compositeDisposable.clear()
+        super.onCleared()
+    }
+}
